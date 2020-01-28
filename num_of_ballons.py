11073fdc0699ad804a93ff4c2e80ad08e9129331
Apiwat Chuaphan <achuaphan@gmail.com>
2020-01-27 20:25:27 -0800

    1189 Maximum Number of Balloons

---
+++ num_of_ballons.py
@@ -0,0 +1,21 @@
+"""
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+ 
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+"""
+
+
+def maxNumberOfBalloons(text):
+    d = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+    for ch in text:
+        if ch in d:
+            d[ch] += 1
+    return min(d['b'], d['a'], d['l']//2, d['o']//2, d['n'])
+
+
+text = "krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw"
+print(maxNumberOfBalloons(text))
