0336c3f7bd34205f9f098c53754c227ec15eaf3b
Apiway Chuaphan <achuaphan@gmail.com>
2020-02-02 00:47:18 -0800

    1341 The K Weakest Rows in a Matrix

---
+++ kweakest_rows.py
@@ -0,0 +1,44 @@
+"""
+Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians),
+return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+
+A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in row j,
+or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row,
+that is, always ones may appear first and then zeros.
+
+Example 1:
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers for each row is:
+row 0 -> 2
+row 1 -> 4
+row 2 -> 1
+row 3 -> 2
+row 4 -> 5
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+"""
+
+
+def kWeakestRows(mat, k):
+    weak = {}
+    weakest = []
+    for index, row in enumerate(mat):
+        weak[index] = row.count(1)
+
+    for (key, v) in sorted(weak.items(), key=lambda kv: kv[1])[:k]:
+        weakest.append(key)
+
+    return weakest
+
+
+mat = [[1, 1, 0, 0, 0], [1, 1, 1, 1, 0],
+       [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 1, 1, 1, 1]]
+k = 3
+print(kWeakestRows(mat, k))
