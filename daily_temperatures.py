f5d93ff09a30f297c684fc345f15fa54f7feb593
Apiwat Chuaphan <achuaphan@gmail.com>
2020-03-10 00:38:25 -0700

    739 Daily Temperatures

---
+++ daily_temperatures.py
@@ -0,0 +1,29 @@
+"""
+Given a list of daily temperatures T, return a list such that, for each day in the input, 
+tells you how many days you would have to wait until a warmer temperature. 
+If there is no future day for which this is possible, put 0 instead.
+
+For example
+given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], 
+your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+"""
+
+
+def dailyTemperatures(T):
+    ans = [0] * len(T)
+    stack = []
+
+    for day, temp in enumerate(T):
+        while stack and stack[-1][0] < temp:
+            curr_day = stack[-1][1]
+            ans[curr_day] = day - curr_day      # days differ
+            stack.pop()
+        stack.append((temp, day))
+
+    return ans
+
+
+T = [55, 38, 53, 81, 61, 93, 97, 32, 43, 78]
+print(dailyTemperatures(T))
